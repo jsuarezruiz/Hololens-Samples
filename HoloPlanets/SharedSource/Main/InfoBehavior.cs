762a1e4c82e23a90af641551facc12bb08088fd6
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-11-05 14:01:20 +0100

    Hololens Planets Sample

---
+++ HoloPlanets/SharedSource/Main/InfoBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.Serialization;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+using WaveEngine.Hololens;
+using WaveEngine.Hololens.Interaction;
+
+namespace HoloPlanets
+{
+    [DataContract]
+    public class InfoBehavior : Behavior
+    {
+        [RequiredComponent]
+        public Transform3D _transform;
+
+        private SpatialState _lastState;
+
+        private HololensService _hololensService;
+        private SpatialInputService _spatialInputManager;
+
+        [DataMember]
+        public bool AllowInfo { get; set; }
+
+        protected override void DefaultValues()
+        {
+            base.DefaultValues();
+
+            this.AllowInfo = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            _hololensService = WaveServices.GetService<HololensService>();
+            _spatialInputManager = WaveServices.GetService<SpatialInputService>();
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            var gesture = _spatialInputManager.SpatialState;
+
+            if (gesture.IsSelected && !_lastState.IsSelected)
+            {
+                this.ShowInfo();
+            }
+
+            _hololensService.SetStabilizationPlane(_transform.Position);
+
+            _lastState = gesture;
+        }
+
+        private void ShowInfo()
+        {
+            Camera3D camera = this.RenderManager.ActiveCamera3D;
+
+            if (AllowInfo && camera != null)
+            {
+                // Show Planet Info
+             
+            }
+        }
+    }
+}
---
+++ HoloPlanets/SharedSource/Main/VoiceCommands.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.Serialization;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Services;
+using WaveEngine.Framework.Sound;
+using WaveEngine.Hololens.Speech;
+
+namespace HoloPlanets
+{
+    [DataContract]
+    public class VoiceCommands : Component
+    {
+        private const string MusicTag = "Music";
+        private const string TurnMusicOn = "Turn Music On";
+        private const string TurnMusicOff = "Turn Music Off";
+
+        private KeywordRecognizerService _keywordService;
+
+        protected override void DefaultValues()
+        {
+            base.DefaultValues();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            _keywordService = WaveServices.GetService<KeywordRecognizerService>();
+
+            if (_keywordService != null)
+            {
+                _keywordService.Keywords = new string[] { TurnMusicOn, TurnMusicOff };
+                _keywordService.Start();
+                _keywordService.OnKeywordRecognized += OnKeywordRecognized;
+            }
+        }
+
+        private void OnKeywordRecognized
+            (KeywordRecognizerResult result)
+        {
+            switch (result.Text)
+            {
+                case TurnMusicOn:
+                    TurnMusic(true);
+                    break;
+                case TurnMusicOff:
+                    TurnMusic(false);
+                    break;
+            }
+        }
+
+        private void TurnMusic(bool state)
+        {
+            foreach (Entity soundEntity in this.Owner.FindAllChildrenByTag(MusicTag))
+            {
+                var emitter = soundEntity.FindComponent<SoundEmitter3D>();
+
+                if (state)
+                {
+                    emitter.Play();
+                    emitter.PlayAutomatically = true;
+                }
+                else
+                {
+                    emitter.PlayAutomatically = false;
+                    WaveServices.SoundPlayer.StopAllSounds();
+                }
+            }
+        }
+    }
+}
